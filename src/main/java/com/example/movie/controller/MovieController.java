3781889936c8b785a5269faecbc1225c1614b8d0
Prashanth-naidu <saiprashanthnaidu68@gmail.com>
2024-06-05 17:50:43 +0000

    commit messge

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -17,4 +17,24 @@ public class MovieController{
     public ArrayList<Movie> getMovies(){
         return s.getMovies();
     }
+
+    @GetMapping("/movies/{movieId}")
+    public Movie getMovieById(@PathVariable("movieId") int movieId){
+        return s.getMovieById(movieId);
+    }
+
+    @PostMapping("/movies")
+    public Movie addMovie(@RequestBody Movie movie){
+        return s.addMovie(movie);
+    }
+
+    @PutMapping("/movies/{movieId}")
+    public Movie updatMovie(@PathVariable("movieId") int movieId, @RequestBody Movie movie){
+        return s.updateMovie(movieId, movie);
+    }
+
+    @DeleteMapping("/movies/{movieId}")
+    public void deleteMovie(@PathVariable("movieId") int movieId){
+        s.deleteMovie(movieId);
+    }
 }
